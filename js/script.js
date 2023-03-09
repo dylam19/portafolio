58eae088ebfff2b3e9af32101969fb8462bd1aa2
dylam19 <105523583+dylam19@users.noreply.github.com>
2023-03-09 18:45:35 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const mainMenuElement = document.getElementById("container-nav");
 const mainMenuItems = document.getElementById("nav-items")
 const blackScreen = document.getElementById("black-screen")
 const toggleClass = document.querySelector('.fa-bars')
+const cardImage = document.getElementsByClassName('project-card-image');
+const projectCardElement = document.getElementsByClassName('project-card');
+const projectTitleElement = document.getElementsByClassName('project-title')
+
+console.log(projectTitleElement)
 
 let boolean = false;
 
@@ -38,3 +43,5 @@ window.addEventListener('scroll', () => {
         mainMenuItems.style.opacity ="100%";
     };
 });
+
+
